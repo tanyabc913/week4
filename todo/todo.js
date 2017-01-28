aa78726bffbb432158deff3b0d4f2e9c7da28243
Tanya Bruskewitz Carlson <t-bruskewitz@kellogg.northwestern.edu>
2017-01-28 12:52:42 -0600

    Add files via upload

---
+++ todo/todo.js
@@ -0,0 +1,12 @@
+// This code runs when the page loads
+$(function() {
+
+  $(".thumbnail").on("click", function(event) {
+    event.preventDefault()
+    var elementThatWasClicked = $(this)
+    console.log(elementThatWasClicked)
+    elementThatWasClicked.parent().remove()
+    // remove the element
+  })
+
+})
